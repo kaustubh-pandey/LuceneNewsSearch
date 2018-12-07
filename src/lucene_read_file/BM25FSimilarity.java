d3e4be225f73f8caeb45c77c63e81295d1c6e514
PRASHANT KUMAR MAHANTA <prashantkumar.m16@iiits.in>
2018-12-07 19:41:01 +0530

    edit ...

--- src/lucene_read_file/BM25FSimilarity.java
+++ src/lucene_read_file/BM25FSimilarity.java
@@ -1,5 +1,5 @@
-package lucene_read_file;/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
+package lucene_read_file;
+/*Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
